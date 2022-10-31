f29b123c630e866108b83635558bca71a64f5887
DinoKrso <86895885+DinoKrso@users.noreply.github.com>
2022-10-31 15:45:33 +0100

    Create Lab3Ex2.java

---
+++ Lab3Ex2.java
@@ -0,0 +1,17 @@
+import java.util.* ; 
+public class Main
+{
+    
+	public static void main(String[] args) {
+	Scanner reader = new Scanner(System.in);  
+    System.out.println("Type your name: ");
+    String name = reader.nextLine(); 
+    int count = 0 ;
+    for(int i = 0 ; i < name.length() ; i++ ){
+        count ++ ; 
+        
+        
+        }
+    System.out.println("Number of characters: "+ count);
+	}
+}
